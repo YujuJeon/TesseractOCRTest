f8e02a4cb9781943c4b30275beebec72a4c3fe55
Yuju <haklim032@gmail.com>
2023-01-03 17:24:21 -0500

    adding addressablee

---
+++ Assets/Scripts/Addressable/SpawnObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private Transform prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Instantiate(prefab);
+        }
+    }
+}
